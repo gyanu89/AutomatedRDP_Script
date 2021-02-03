be752ec91920c818968911e7038c5ac2b23cac9d
Gyanendra Dwivedi <36406569+gyanu89@users.noreply.github.com>
2021-02-03 10:47:31 -0500

    Add files via upload
    Using this script no need to enter user name and password on each RDP.

---
+++ Auto_RDP.ps1
@@ -0,0 +1,9 @@
+﻿cmdkey /list | ForEach-Object{if($_ -like "*target=TERMSRV/*"){cmdkey /del:($_ -replace " ","" -replace "Target:","")}}
+$user = $null
+$server = $null
+$serverPassword = $null
+$server = Read-Host "Enter Server Name"
+$user = "na\admin_gkumar31"
+$serverPassword = "Cobol@123$"
+cmdkey /generic:TERMSRV/$server /user:$user /pass:$serverPassword
+mstsc /v:$server /w:800 /h:600
